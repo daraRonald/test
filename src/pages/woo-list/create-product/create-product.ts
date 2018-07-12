a9b90e4e1f0bda799e6ae101a6e3e40f19cb6c29
poeei_MM <poeeiaerial100@gmail.com>
2018-07-12 11:30:36 +0630

    img5

--- src/pages/woo-list/create-product/create-product.ts
+++ src/pages/woo-list/create-product/create-product.ts
@@ -4,6 +4,7 @@ import { WordpressProvider} from '../../../providers/wordpress/wordpress';
 import { ImagePicker } from '@ionic-native/image-picker';
 import { Base64 } from '@ionic-native/base64';
 import { ImageProvider } from '../../../providers/image/image';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the CreateQuotePage page.
@@ -32,7 +33,8 @@ export class CreateProductPage {
 			  public actionSheetCtrl: ActionSheetController,
 			  private imagePicker: ImagePicker,
 			  private _IMG: ImageProvider,
-			  private base64: Base64) {}
+			  private base64: Base64,
+			  private _CAMERA : Camera) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateProductPage');
@@ -46,19 +48,18 @@ export class CreateProductPage {
 			  text: 'Media Library',
 			  role: 'media',
 			  handler: () => {
-				alert('Media clicked');
-			  }
-			},
-			{
-			  text: 'Upload File',
-			  handler: () => {
 				this._IMG.selectImage()
 				  .then((data) =>
 				  {
 					 this.image = data;
 					 alert(this.image);
 				  });
-				
+			  }
+			},
+			{
+			  text: 'Upload File',
+			  handler: () => {
+				this.selectImage();
 			  }
 			 },
 			 {
@@ -80,9 +81,58 @@ export class CreateProductPage {
 	  
   }
 
-  upload() {
-	this._IMG.uploadImages(this.image);
-  }
+  selectImage() {
+      
+         let cameraOptions : CameraOptions = {
+             sourceType         : this._CAMERA.PictureSourceType.PHOTOLIBRARY,
+             destinationType    : this._CAMERA.DestinationType.DATA_URL,
+             quality            : 100,
+             targetWidth        : 320,
+             targetHeight       : 240,
+             encodingType       : this._CAMERA.EncodingType.JPEG,
+             mediaType          : this._CAMERA.MediaType.PICTURE,
+             correctOrientation : true
+         };
+
+         this._CAMERA.getPicture(cameraOptions)
+         .then((data) =>
+         {
+            this.image = "data:image/png;base64," + data;
+         });
+         
+		
+	  
+      
+   }
+   
+  uploadFile() {
+  
+	  let loader = this.loadingCtrl.create({
+		content: "Uploading..."
+	  });
+	  loader.present();
+	  const fileTransfer: FileTransferObject = this.transfer.create();
+
+	  let options: FileUploadOptions = {
+		fileKey: 'ionicfile',
+		fileName: 'ionicfile',
+		chunkedMode: false,
+		mimeType: "image/jpeg",
+		headers: {}
+	  }
+
+	  fileTransfer.upload(this.imageURI, 'http://192.168.0.7:8080/api/uploadImage', options)
+		.then((data) => {
+		console.log(data+" Uploaded Successfully");
+		this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg"
+		loader.dismiss();
+		this.presentToast("Image uploaded successfully");
+	  }, (err) => {
+		console.log(err);
+		loader.dismiss();
+		this.presentToast(err);
+	  });
+	}
   
   onCreateProduct(){
     this.wordpressProvider.createProduct(this.name, this.content,this.price,this.sale_price,this.image).subscribe(data => {
